f4211c627d9e3ea8daa9b1b7f6227fc1595e12aa
erDeep640390 <erdeep64930@gmail.com>
2023-09-11 16:04:39 +0530

    basic folder structure has been done

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-
-
+import { Routes, Route } from "react-router-dom";
+import { HomePage, AboutPage, ContactPage, ServicePage, BlogPage, PageNotFound } from "./pages/index"
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+
+
+      <Routes>
+
+
+        <Route path="/" element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/service" element={<ServicePage />} />
+        <Route path="/blog" element={<BlogPage />} />
+
+        <Route path="*" element={<PageNotFound />} />
+
+
+
+      </Routes>
+    </div>
   )
 }
 
